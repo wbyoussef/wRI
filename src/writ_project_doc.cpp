128aa071ae586bccef06336515abb7cb9499ad0a
wbyoussef <walid.benyoussef@gmail.com>
2015-12-12 17:39:54 +0100

    add project's document

---
+++ src/writ_project_doc.cpp
@@ -0,0 +1,7 @@
+#include "writ_project_doc.h"
+
+writ_project_doc::writ_project_doc()
+{
+
+}
+
---
+++ src/writ_project_doc.h
@@ -0,0 +1,11 @@
+#ifndef WRIT_PROJECT_DOC_H
+#define WRIT_PROJECT_DOC_H
+
+
+class writ_project_doc
+{
+public:
+    writ_project_doc();
+};
+
+#endif // WRIT_PROJECT_DOC_H
