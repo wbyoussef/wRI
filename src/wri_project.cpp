632219f7cbdb12da20336483a1716d1117b74a8e
wbyoussef <walid.benyoussef@gmail.com>
2015-12-13 11:29:03 +0100

    enhancement to MDI mechanisms

--- src/wri_project.cpp
+++ src/wri_project.cpp
@@ -6,6 +6,11 @@ wRI_Project::wRI_Project(QWidget *parent) :
     ui(new Ui::wRI_Project)
 {
     ui->setupUi(this);
+    this->ui->gridLayout->setMargin(0);
+    this->ui->gridLayout_2->setMargin(0);
+    this->ui->gridLayout->setSpacing(0);
+    this->ui->gridLayout_2->setSpacing(0);
+
 }
 
 wRI_Project::~wRI_Project()
